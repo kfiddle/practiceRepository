0fd936de741bb0530eaa4a30542271917648594b
kfiddle <kenjfiddle@gmail.com>
2020-10-21 17:27:37 -0400

    Some more perfect code

--- randomJS.js
+++ randomJS.js
@@ -1,2 +1,5 @@
 const myHeader = document.querySelector("h1");
+const secondHeader = document.querySelector('h2');
+
 myHeader.innerText = "My former header was stupid";
+secondHeader.innerText = "But now for a new sensational headline. Again, the other was dumb."
